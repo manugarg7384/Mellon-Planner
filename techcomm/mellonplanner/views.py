9fd0f6b206a07dd16bfb6d703a6322af15db52b5
Manu Garg <manug@andrew.cmu.edu>
2015-04-24 20:57:29 -0400

    Added semester option, prefernce

--- techcomm/mellonplanner/views.py
+++ techcomm/mellonplanner/views.py
@@ -46,6 +46,16 @@ def getschedules(request):
         errors.append('Minimum units is required')
     if not 'maxunits' in request.POST or not request.POST['maxunits']:
         errors.append('Maximum units is required')
+    semester = request.POST['semester']
+    pref = request.POST['preference']
+    if(int(semester) == 0):
+        context['semester'] = 'Fall'
+    elif(int(semester) == 1):
+        context['semester'] = 'Spring'
+    elif(int(semester) == 2):
+        context['semester'] = 'Summer 1/ALL'
+    elif(int(semester) == 3):
+        context['semester'] = 'Summer 2'       
     if errors:
         return render(request, 'index.html', context)
     if(int(request.POST['minunits']) < 0 or
@@ -55,7 +65,11 @@ def getschedules(request):
     # Should check for validity of classes here, and add errors if any
     # Then call the backend functions
     try:
-        schedules = getAllSchedules(list_of_classes)
+        print(semester)
+        schedules = getAllSchedules(list_of_classes, int(semester),
+                                    request.POST['minunits'],
+                                    request.POST['maxunits'],
+                                    str(pref))
 
     except Exception:
         context['errors'] += "Something went wrong with course names. May not exist."
@@ -124,67 +138,3 @@ def home(request):
     context = {}
     # context.update(csrf(request))
     return render(request, 'index.html', context)
-
-# def register(request):
-#     context = {}
-#     context.update(csrf(request))
-
-#     # Just display the registration form if this is a GET request
-#     if request.method == 'GET':
-#         return render(request, 'Login_Page.html', context)
-
-#     errors = []
-#     context['errors'] = errors
-
-#     # Checks the validity of the form data
-#     if not 'username1' in request.POST or not request.POST['username1']:
-# 	errors.append('Username is required.')
-#     else:
-#         # Save the username in the request context to re-fill the username
-#         # field in case the form has errrors
-# 	context['username'] = request.POST['username1']
-#     if not 'username2' in request.POST or not request.POST['username2']:
-# 	errors.append('Confirm username is required.')
-
-#     if 'username1' in request.POST and 'username2' in request.POST \
-#        and request.POST['username1'] and request.POST['username2'] \
-#        and request.POST['username1'] != request.POST['username2']:
-# 	errors.append('Usernames did not match.')
-
-#     if not 'password1' in request.POST or not request.POST['password1']:
-# 	errors.append('Password is required.')
-#     if not 'password2' in request.POST or not request.POST['password2']:
-# 	errors.append('Confirm password is required.')
-
-#     if 'password1' in request.POST and 'password2' in request.POST \
-#        and request.POST['password1'] and request.POST['password2'] \
-#        and request.POST['password1'] != request.POST['password2']:
-# 	errors.append('Passwords did not match.')
-
-#     if len(User.objects.filter(username = request.POST['username1'])) > 0:
-# 	errors.append('There is already an account linked to that username')
-
-#     if errors:
-#         return render(request, 'Login_Page.html', context)
-
-#     # Creates the new user from the valid form data
-#     new_user = User.objects.create_user(username=request.POST['username1'], \
-#                                         password=request.POST['password1'])
-#     new_user.save()
-
-#     username = new_user.get_username()
-#     fname = ''
-#     lname = ''
-#     if 'fname' in request.POST and request.POST['fname']:
-#         fname = request.POST['fname']
-#     if 'lname' in request.POST and request.POST['lname']:
-#         lname = request.POST['lname']
-#     new_user_profile = Profile(user=new_user, first_name=fname, \
-#                                last_name=lname, username=username)
-#     new_user_profile.save()
-
-#     # Logs in the new user and redirects to his/her profile
-#     new_user = authenticate(username=request.POST['username1'], \
-#                             password=request.POST['password1'])
-#     login(request, new_user)
-#     return redirect('/myschedule/')
